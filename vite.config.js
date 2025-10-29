dbb1ad766e9c42ba3239930f8ba0f8773605ae2a
REVDEVHQ <jay@revdevhq.com>
2025-10-29 00:05:19 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(process.cwd(), 'index.html')
+    }
+  }
+})vite.config.js
